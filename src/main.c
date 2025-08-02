fe2abb5ea45c4efda75b990d234e861de1744aae
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-08-02 21:58:01 +0200

    Add .gitignore

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
+#include <dirent.h>
 #include <stdio.h>
 
 int
+walk(const char *const base_path) {
+    struct dirent *file;
+    DIR *base = opendir(base_path);
+
+    if (base == NULL) {
+        return -1;
+    }
+
+    while ((file = readdir(base)) != NULL) {
+        if (file->d_type == DT_DIR) {
+            walk(file->d_name);
+            printf("%s\n", file->d_name);
+        }
+    }
+
+    return 0;
+}
+
+int
 main() {
-    printf("Hello, World!\n");
+    walk(".");
 }
