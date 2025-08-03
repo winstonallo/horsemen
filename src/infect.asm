c2ce89052554dde1f391317ba72210fd8a39c379
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-08-03 17:34:42 +0200

    Add asm code with elf_ident_check

---
+++ src/infect.asm
@@ -0,0 +1,176 @@
+global _start
+; syscalls
+SYS_READ:       equ 0
+SYS_WRITE:      equ 1
+SYS_OPEN:       equ 2
+SYS_CLOSE:      equ 3
+SYS_MPROTECT:   equ 10
+SYS_EXIT:       equ 60
+
+; open flags
+O_RDONLY:   equ 0
+O_WRONLY:   equ 1
+O_RDWR:     equ 2
+
+; mprotect flags
+PROT_READ:  equ 0x01
+PROT_WRITE: equ 0x02
+PROT_EXEC:  equ 0x04
+
+; ELF
+; e_ident
+EI_NIDENT:      equ 16
+EI_MAG0:        equ 0
+ELFMAG0:        equ 0x7f
+EI_MAG1:        equ 1
+ELFMAG1:        equ 'E'
+EI_MAG2:        equ 2
+ELFMAG2:        equ 'L'
+EI_MAG3:        equ 3
+ELFMAG3:        equ 'F'
+EI_CLASS:       equ 4
+ELFCLASS64:     equ 2
+EI_DATA:        equ 5
+ELFDATANONE:    equ 0
+EI_VERSION:     equ 6
+EV_CURRENT:     equ 1
+EI_PAD:         equ 9
+
+; e_type
+ET_NONE:    equ 0
+ET_REL:     equ 1
+ET_EXEC:    equ 2
+ET_DYN:     equ 3
+ET_CORE:    equ 4
+ET_NUM:     equ 5
+
+struc Elf64_Ehdr
+    .e_ident        resb EI_NIDENT
+    .e_type         resw 1
+    .e_machine      resw 1
+    .e_version      resd 1
+    .e_entry        resq 1
+    .e_phoff        resq 1
+    .e_shoff        resq 1
+    .e_flags        resd 1
+    .e_ehsize       resw 1
+    .e_phentsize    resw 1
+    .e_phnum        resw 1
+    .e_shentsize    resw 1
+    .e_shnum        resw 1
+    .e_shstrndx     resw 1
+endstruc
+
+struc Elf64_Shdr
+    .sh_name        resd 1
+    .sh_type        resd 1
+    .sh_flags       resq 1
+    .sh_addr        resq 1
+    .sh_offset      resq 1
+    .sh_size        resq 1
+    .sh_link        resw 1
+    .sh_info        resw 1
+    .sh_addralign   resq 1
+    .sh_entsize     resq 1
+endstruc
+
+_start:
+    mov rax, SYS_OPEN
+    mov rsi, O_RDONLY
+    lea rdi, [rel file_path]
+    syscall
+    cmp eax, 0
+    jl error
+
+    mov r8, rax
+
+    sub rsp, 16
+
+    mov rax, SYS_READ
+    mov rdi, r8
+    lea rsi, [rsp]
+    mov rdx, 16
+    syscall
+    cmp eax, 0
+    jl error
+
+    lea rdi, [rsp]
+    call elf64_ident_check
+
+    add rsp, 16
+
+    mov rax, SYS_CLOSE
+    mov rdi, r8
+    syscall
+
+    mov rax, SYS_EXIT
+    mov rdi, 0
+    syscall
+
+elf64_ident_check:
+    cmp BYTE [rdi + EI_MAG0], ELFMAG0
+    jne not_elf
+    cmp BYTE [rdi + EI_MAG1], ELFMAG1
+    jne not_elf
+    cmp BYTE [rdi + EI_MAG2], ELFMAG2
+    jne not_elf
+    cmp BYTE [rdi + EI_MAG3], ELFMAG3
+    jne not_elf
+    cmp BYTE [rdi + EI_CLASS], ELFCLASS64
+    jne not_64_bit
+    cmp BYTE [rdi + EI_DATA], ELFDATANONE
+    je not_elf
+    cmp BYTE [rdi + EI_VERSION], EV_CURRENT
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD], 0
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD + 1], 0
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD + 2], 0
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD + 3], 0
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD + 4], 0
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD + 5], 0
+    jne not_elf
+    cmp BYTE [rdi + EI_PAD + 6], 0
+    jne not_elf
+    ret
+
+error:
+    mov rax, SYS_EXIT
+    mov rdi, 1
+    syscall
+
+success:
+    mov rax, SYS_EXIT
+    mov rdi, 0
+    syscall
+
+not_elf:
+    add rsp, 16
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    lea rsi, [rel not_elf_msg]
+    mov rdx, 11
+    syscall
+
+    jmp success
+
+not_64_bit:
+    add rsp, 16
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    lea rsi, [rel not_64_bit_msg]
+    mov rdx, 17
+    syscall
+
+    jmp success
+
+not_64_bit_msg:
+    db "Not a 64 bit ELF",10
+not_elf_msg:
+    db "Not an ELF",10
+file_path:
+    db "Famine", 0
