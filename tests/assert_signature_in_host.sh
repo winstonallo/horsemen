817379228a7664e6f473cb8d682204065d1af717
winstonallo <arthurbiedchar@gmail.com>
2025-08-10 13:35:10 +0200

    Suppress Famine stderr in tests

--- tests/assert_signature_in_host.sh
+++ tests/assert_signature_in_host.sh
@@ -3,5 +3,5 @@
 make > /dev/null
 echo "int main() { return 0; }" > /tmp/test.c
 cc /tmp/test.c -o /tmp/test/sample
-./Famine > /dev/null
+./Famine > /dev/null 2>&1
 strings /tmp/test/sample | grep "abied-ch" > /dev/null
