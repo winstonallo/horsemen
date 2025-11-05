93bd6475e2e59ba14a3ec4d67e0ab722d912f1f7
fbruggem <fbruggem@proton.me>
2025-11-05 13:00:05 +0100

    pin nix version and cleanup

--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
 
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+ pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/a84b0a7c509bdbaafbe6fe6e947bdaa98acafb99.tar.gz";
+    sha256 = "0m8zrg4rp5mx5v9ar91ncnjhagmcrd3y9h56y48swan6a8gwpq52";
+  }) {};
+in
 pkgs.mkShell {
   name = "dev-shell";
 
@@ -17,8 +21,6 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    export DEV_ENV=1
-
-    #sudo sysctl -w vm.mmap_min_addr=0
+    export PS1="\W> "
   '';
 }
