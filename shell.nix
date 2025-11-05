4a01b2c44e37e87df922abcd4172d3fb391f51fb
fbruggem <fbruggem@proton.me>
2025-11-05 13:09:21 +0100

    cleanp

--- shell.nix
+++ shell.nix
@@ -1,10 +1,6 @@
 
-let
- pkgs = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a84b0a7c509bdbaafbe6fe6e947bdaa98acafb99.tar.gz";
-    sha256 = "0m8zrg4rp5mx5v9ar91ncnjhagmcrd3y9h56y48swan6a8gwpq52";
-  }) {};
-in
+{ pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
   name = "dev-shell";
 
@@ -16,7 +12,6 @@ pkgs.mkShell {
     xxd
     python3
     clang-tools
-    glibc.dev
     valgrind
   ];
 
