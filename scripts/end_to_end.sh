24395bb9e4a45fb0e8d4eeb93260fab13f97e935
Arthur Bied-Charreton <arthur@biedcharreton.com>
2025-11-11 14:38:42 +0100

    Add more tests verifying that infected binary pass on the virus as well

--- scripts/end_to_end.sh
+++ scripts/end_to_end.sh
@@ -8,28 +8,40 @@ hash=1065a3d95aa5451b
 echo 'int main() { while (1) ; }' > /tmp/foo.c && gcc /tmp/foo.c -o /tmp/$hash
 /tmp/$hash > /dev/null 2>&1 &
 ./Pestilence > /dev/null 2>&1
-strings /tmp/test/ls | grep Pestilence > /dev/null && echo "Pestilence should not infect when 1065a3d95aa5451b is running" && exit 1
-echo "Pestilence did not infect hosts when 1065a3d95aa5451b is running"
+strings /tmp/test/ls | grep Pestilence > /dev/null && echo "[KO] Pestilence should not infect when 1065a3d95aa5451b is running" && exit 1
+echo "[OK] Pestilence did not infect hosts when 1065a3d95aa5451b is running"
 kill -9 $(pgrep -f $hash) > /dev/null 2>&1
 
 gdb -q --batch -ex "run" --args ./Pestilence > /dev/null 2>&1
-strings /tmp/test/ls | grep Pestilence > /dev/null && echo "Pestilence should not infect when ran in GDB" && exit 1
-echo "Pestilence did not infect hosts when ran with GDB"
+strings /tmp/test/ls | grep Pestilence > /dev/null && echo "[KO] Pestilence should not infect when ran in GDB" && exit 1
+echo "[OK] Pestilence did not infect hosts when ran with GDB"
 
 ./Pestilence
-strings /tmp/test/ls | grep Pestilence > /dev/null || { echo "Pestilence should infect when ran alone"; exit 1; }
-echo "Pestilence infects binaries in /tmp/test"
+strings /tmp/test/ls | grep Pestilence > /dev/null || { echo "[KO] Pestilence should infect when ran alone"; exit 1; }
+echo "[OK] Pestilence infects binaries in /tmp/test"
 
 rm -rf /tmp/{test,test2}
 mkdir /tmp/test2
 cp $(which ls) /tmp/test2
 chmod +w /tmp/test2/ls
 ./Pestilence
-strings /tmp/test2/ls | grep Pestilence > /dev/null || { echo "Pestilence should infect binaries in /tmp/test2"; exit 1; }
-echo "Pestilence infects binaries in /tmp/test2"
+strings /tmp/test2/ls | grep Pestilence > /dev/null || { echo "[KO] Pestilence should infect binaries in /tmp/test2"; exit 1; }
+echo "[OK] Pestilence infects binaries in /tmp/test2"
+
+mv /tmp/test2/ls /tmp
+./scripts/copy_hosts.sh > /dev/null
+/tmp/ls > /dev/null
+strings /tmp/test/ls | grep Pestilence > /dev/null || { echo "[KO] Infected binary should infect other binaries in /tmp/test"; exit 1; }
+echo "[OK] Infected binary infects other binaries in /tmp/test"
+
+cp $(which ls) /tmp/test2
+chmod +w /tmp/test2/ls
+/tmp/ls > /dev/null
+strings /tmp/test2/ls | grep Pestilence > /dev/null || { echo "[KO] Infected binary should infect other binaries in /tmp/test2"; exit 1; }
+echo "[OK] Infected binary infects other binaries in /tmp/test2"
 
 valgrind /tmp/test/ls > /dev/null 2>&1 | grep "Stopped" > /dev/null && echo "The valgrind ./ls bullshit is happening again" && exit 1
-echo "/tmp/test/ls is working with valgrind"
+echo "[OK] /tmp/test/ls is working with valgrind"
 
 rm -rf /tmp/{test,test2}
 echo "All tests passed"
